f392eea2e2f35eef47a42385c76b36a09525f435
asaccone <asaccone@uchicago.edu>
2020-03-08 00:18:34 +0000

    Improved Food Item Page

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -14,19 +14,16 @@ class FoodItemsController < ApplicationController
 
   def create
     @food_item = FoodItem.new
-    @food_item.fridge_id = params.fetch("query_fridge_id")
     @food_item.owner_id = params.fetch("query_owner_id")
+    @food_item.item_name = params.fetch("query_item_name")
     @food_item.food_type = params.fetch("query_food_type")
     @food_item.expiration_date = params.fetch("query_expiration_date")
-    @food_item.message1_sent = params.fetch("query_message1_sent", false)
-    @food_item.message2_sent = params.fetch("query_message2_sent", false)
-    @food_item.message3_sent = params.fetch("query_message3_sent", false)
 
     if @food_item.valid?
       @food_item.save
-      redirect_to("/food_items", { :notice => "Food item created successfully." })
+      redirect_to("/", { :notice => "Food item created successfully." })
     else
-      redirect_to("/food_items", { :notice => "Food item failed to create successfully." })
+      redirect_to("/", { :notice => "Food item failed to create successfully." })
     end
   end
 
