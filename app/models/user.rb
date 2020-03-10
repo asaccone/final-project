1a4c7529e3a752901557b4d7f0782ddf44bab5a9
asaccone <asaccone@uchicago.edu>
2020-03-10 03:02:32 +0000

    Added time column

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 #  email           :string
 #  password_digest :string
 #  phone_number    :string
+#  sendtime        :time
 #  username        :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_002947) do
+ActiveRecord::Schema.define(version: 2020_03_10_025841) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_03_08_002947) do
     t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.time "sendtime"
   end
 
 end
